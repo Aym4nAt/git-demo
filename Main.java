75286267f4b16fe63782c3f13248aac01d64d5e4
Ayman <aturkistani0049@stu.kau.edu.sa>
2024-11-22 14:35:06 +0300

    Forth Commit by Github

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 class Main {
 public static void main(String[] args) {
+//Ayman Turkistani 
 int first = 10;
 int second = 20;
 // add two numbers
 int sum = first + second;
-
-
 System.out.println(first + " + " + second + " = " + sum);
 }
 }
